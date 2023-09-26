638e202866cb605e406978d12453907bc94b6410
dziuba <dziuba@NK-W12-21>
2023-09-26 15:39:01 +0300

    Add project files.

---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,35 @@
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Two of clubs";
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Queen of diamonds";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "King of hearts";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
